abe09dca3360d98465fe2aab36c39c2a88bcc3fb
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-09-28 21:17:16 +0100

    Add parameters
    Added parameters for the game. Began coding the conditions.

--- javascript.js
+++ javascript.js
@@ -9,3 +9,15 @@ function playerSelect() {
     let selection = prompt("Rock, paper, scissors - what do you choose?");
     return selection;
 }
+
+const one = "paper" > "rock";
+const two = "rock" > "scissors";
+const three = "scissors" > "paper";
+
+function test() {
+    if ( choice > selection ) {
+        alert("You lose!");
+    } else {
+        alert("You win!");
+    }
+}
