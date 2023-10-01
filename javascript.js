a7440348650534356fe9ecdcb72ba1bb2f2ba3f4
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-10-01 14:01:57 +0100

    begin gameRound function

--- javascript.js
+++ javascript.js
@@ -3,12 +3,19 @@ let choice = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   console.log(choice[Math.floor(Math.random() * choice.length)]);
 }
-getComputerChoice();
 //Function to get player's choice.
 function playerSelection() {
     let selection = prompt("Rock, paper, scissors - what do you choose?");
-    return `You chose ${selection}`;
+    console.log(`You chose ${selection}`);
 }
 //setting up variable for the gameRound function
 const computer = getComputerChoice();
 const player = playerSelection();
+
+function gameRound(computer, player) {
+    if (player == "rock" && computer == "paper") {
+        return ("You lose! Paper beats rock!");
+    } else {
+        return ("Yay!");
+    }
+}
