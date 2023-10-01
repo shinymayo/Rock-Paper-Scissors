8285586601b16961ddf3883a0b650ffca8bd7a56
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-10-01 13:36:48 +0100

    Clean code
    Deleted non functional lines of code and added
    comments to the code for future use.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,14 @@
+//Function to get computer's choice
 let choice = ["rock", "paper", "scissors"];
-
 function getComputerChoice() {
   console.log(choice[Math.floor(Math.random() * choice.length)]);
 }
 getComputerChoice();
-
+//Function to get player's choice.
 function playerSelection() {
     let selection = prompt("Rock, paper, scissors - what do you choose?");
     return `You chose ${selection}`;
 }
-
-const one = "paper" > "rock";
-const two = "rock" > "scissors";
-const three = "scissors" > "paper";
-
-function test() {
-    if ( choice > selection ) {
-        alert("You lose!");
-    } else {
-        alert("You win!");
-    }
-}
+//setting up variable for the gameRound function
+const computer = getComputerChoice();
+const player = playerSelection();
