deb2e51a6e875cce78800decc4b3d9edff69c0ee
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-10-03 10:33:59 +0100

    write gameRound function

--- javascript.js
+++ javascript.js
@@ -15,7 +15,21 @@ const player = playerSelection();
 function gameRound(computer, player) {
     if (player == "rock" && computer == "paper") {
         return ("You lose! Paper beats rock!");
-    } else {
-        return ("Yay!");
+    } else if (player == "paper" && computer == "rock") {
+        return ("You win! Paper beats rock!");
+    } else if (player == "rock" && computer == "scissors") {
+        return ("You win! Rock beats scissors!");
+    } else if (player == "scissors" && computer == "rock") {
+        return ("You lose! Rock beats scissors");
+    } else if (player == "paper" && computer == "scissors") {
+        return ("You lose! Scissors beat paper");
+    } else if (player == "scissors" && computer == "paper") {
+        return ("You win! Scissors beat paper");
+    } else if (player == "rock" && computer == "rock") {
+        return ("It's a tie!");
+    } else if (player == "paper" && computer == "paper") {
+        return ("It's a tie. Oh well...");
+    } else if (player == "scissors" && computer == "scissors") {
+        return ("It's a tie! Try again");
     }
 }
